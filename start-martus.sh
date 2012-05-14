1403d2c1946c2e52fb2920bafe01f639ba3966e9
Jacob Appelbaum <jacob@appelbaum.net>
2012-05-14 15:58:15 -0400

    fix paths

--- start-martus.sh
+++ start-martus.sh
@@ -4,17 +4,17 @@
 #   /usr/lib/jvm/java-1.6.0-openjdk/bin/java \
 #    -Xbootclasspath/p:`pwd`/client-release/LibExt/bc-jce.jar -jar client-release/martus.jar
 
-/usr/lib/jvm/java-1.6.0-openjdk/bin/java -Xbootclasspath/p:`pwd`/../LibExt/bc-jce.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/bc-jce.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/icu4j_3_2_calendar.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/js.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/layouts.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/velocity-1.4-rc1.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/xmlrpc-1.2-b1.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/bcprov-jdk14-135.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/InfiniteMonkey.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/junit.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/persiancalendar.jar \
- -Xbootclasspath/p:`pwd`/../LibExt/velocity-dep-1.4-rc1.jar \
- -jar martus.jar
+/usr/lib/jvm/java-1.6.0-openjdk/bin/java -Xbootclasspath/p:`pwd`/client-release/LibExt/bc-jce.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/bc-jce.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/icu4j_3_2_calendar.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/js.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/layouts.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/velocity-1.4-rc1.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/xmlrpc-1.2-b1.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/bcprov-jdk14-135.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/InfiniteMonkey.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/junit.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/persiancalendar.jar \
+ -Xbootclasspath/p:`pwd`/client-release/LibExt/velocity-dep-1.4-rc1.jar \
+ -jar `pwd`/client-release/martus.jar
 
